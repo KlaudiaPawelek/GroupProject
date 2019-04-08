604070c8e23b268bb69b90346e439b26e811c208
kac723 <44072790+kac723@users.noreply.github.com>
2019-04-08 12:08:33 +0100

    Code clean up. Changed output text file format

--- bubble.cpp
+++ bubble.cpp
@@ -22,24 +22,17 @@ vector<Rect> dropsLC;
 vector <double> diameters_in_mikrometers;      // diameters in mikrometers unit
 double calibration_factor;
 string name_of_classifier;
-Point c(0, 0), S(0, 0), test(0, 0);
 int radiusS = 0;
-int difx, dify;
 
 Mat img;
 Mat img1;
 CascadeClassifier cascade;
-const string windowName = "Supervising";
 int key;
 int number_of_drops;
 
-// File with Diameters in pixel unit
+// File with output information
 std::ofstream file;
-string name_of_file;
-
-//file with Diameters in mikrometers
-std::ofstream file1;
-string name_of_file1;
+string name_of_file;;
 
 
 bool keepProcessing;
@@ -48,67 +41,6 @@ int temp;
 
 double d2, d3, SMD;              // to calculate Sauter mean diameter
 
-
-								 // function calculates center and radius of circle having three points on the circumference
-void calculateCenter(Point P1, Point P2, Point P3)
-{
-	// coordinates of the first point
-	int p1x = P1.x;
-	int p1y = -P1.y;
-
-	// coordinates of the second point
-	int p2x = P2.x;
-	int p2y = -P2.y;
-
-	// coordinates of the third point
-	int p3x = P3.x;
-	int p3y = -P3.y;
-
-	int p1x2 = P1.x*P1.x;
-	int p1y2 = P1.y*P1.y;
-
-	int p2x2 = P2.x*P2.x;
-	int p2y2 = P2.y*P2.y;
-
-	int p3x2 = P3.x*P3.x;
-	int p3y2 = P3.y*P3.y;
-
-	// calculating coordinates of the circle's center
-	S.y = 0.5 * ((-p1x * p3x2 - p1x * p3y2 + p1x * p2x2 + p1x * p2y2 + p2x * p3x2 + p2x * p3y2 - p2x2 * p3x - p2y2 * p3x + p1x2 * p3x - p1x2 * p2x + p1y2 * p3x - p1y2 * p2x) / (p1y * p3x - p1y * p2x - p2y * p3x - p3y * p1x + p3y * p2x + p2y * p1x));
-	S.x = (p3x2 - p2x2 + p3y2 - p2y2 + 2 * S.y*p2y - 2 * S.y*p3y) / (2 * (p3x - p2x));
-
-	// calculating radius of the circle
-	radiusS = sqrt((p1x - S.x)*(p1x - S.x) + (p1y - S.y)*(p1y - S.y));
-	S.y *= -1;
-}
-
-
-// function supports mouse events
-void supervisedCircles(int event, int x, int y, int flags, Mat* img)
-{
-	int row = y; // y-axis is image rows (down the side)
-	int col = x; // x-axis is image columns (along the top)
-
-	switch (event)
-	{
-	case CV_EVENT_LBUTTONDOWN: /// left mouse click
-
-		c.x = x;
-		c.y = y;
-		points.push_back(c);                               // adding points to the vector 
-		circle(*img, Point(x, y), 5, Scalar(0, 255, 0), 2); // drawing little, green circles on the image
-		break;
-
-
-	case CV_EVENT_RBUTTONDOWN: // right mouse click
-
-		test.x = x;
-		test.y = y;
-		points_to_remove.push_back(test);                 // adding points to the vector
-		circle(*img, Point(x, y), 10, Scalar(0, 0, 255)); // drawing red circles on the image
-		break;
-	}
-}
 /*******************************************************************/
 //Raise to power transform and logarithm transform.
 Mat r2pTransform(Mat input)
@@ -194,33 +126,22 @@ int main(int argc, const char **argv) {
 	//cout << "To remove droplet use right mouse button: click inside all droplets that should be removed and submit your choice by clicking 'e'" << endl;
 	//cout << "n - Next image (automatically saving image and all your changes" << endl;
 	//cout << "r - refresh (you can check what will be saved)" << endl;
-
-	float sobelx_data1[16] = { 0,1,1,0,1,0,0,1,-1,0,0,-1,0,-1,-1,0 };
-	Mat sobelx1 = Mat(4, 4, CV_32F, sobelx_data1);
-
-	float sobelx_data2[16] = { 0,-1,-1,0,-1,0,0,-1,1,0,0,1,0,1,1,0 };
-	Mat sobelx2 = Mat(4, 4, CV_32F, sobelx_data2);
-
-	float sobelx_data3[16] = { 0,1,-1,0,1,0,0,-1,1,0,0,-1,0,1,-1,0 };
-	Mat sobelx3 = Mat(4, 4, CV_32F, sobelx_data3);
-
-	float sobelx_data4[16] = { 0,-1,1,0,-1,0,0,1,-1,0,0,1,0,-1,1,0 };
-	Mat sobelx4 = Mat(4, 4, CV_32F, sobelx_data4);
-
+	vector<int> saveParams;
+	saveParams.push_back(IMWRITE_PNG_COMPRESSION);
+	saveParams.push_back(9);
 	calibration_factor = atof(argv[1]);  // calibration factor defined by user
 	name_of_classifier = argv[3];        // name of classifier that should be used (for example Haar5.xml)
 	number_of_drops = 0;                 // counter of droplets on all images
 	vector<Mat> imagesAfterLC(argc - 4);
+	auto start = chrono::system_clock::now();
 	imagesAfterLC= LensCleaning(argc, argv);
 
 										 // Loop through all images in the input folder, it starts from 4 because path to images is 5th argument on the command window
-	for (int k = 4; k < argc; k++) {
-
-		//cout << "Argc" << argc << endl;
+	for (int k = 4; k < argc; k++) 
+	{
 
 		path = argv[2];                                        // path to output folder
-		name_of_file = path + "\\diametersInPixels.txt";       // creating file with diameters in pixel unit
-		name_of_file1 = path + "\\diametersInMikroMeters.txt"; // creating file with diameters in mikrometers
+		name_of_file = path + "\\outputFile.txt";       // creating file with diameters in pixel unit
 		keepProcessing = true;
 
 		Mat imgLC = imagesAfterLC[k - 4];
@@ -233,8 +154,6 @@ int main(int argc, const char **argv) {
 		if(!imgLC.empty())
 		imgLCr2p = r2pTransform(imgLC);
 
-
-
 		if (img.empty())
 		{
 			cout << "Failed to open image " << argv[k] << endl;
@@ -244,16 +163,6 @@ int main(int argc, const char **argv) {
 		{
 			cout << "Failed to open image " << argv[k] << endl;
 			return -1;
-		}
-	 // If this is the first turn of the loop, programme save information about calibration factor
-		if (k == 4) {
-			file.open(name_of_file, std::ofstream::app);
-			file << "Calibration factor: " << calibration_factor << " mikro meters/pixel" << endl;
-			file.close();
-
-			file1.open(name_of_file1, std::ofstream::app);
-			file1 << "Calibration factor: " << calibration_factor << " mikro meters/pixel" << endl;
-			file1.close();
 		}
 
 
@@ -294,20 +203,10 @@ int main(int argc, const char **argv) {
 				cout << "Failed to open file " << name_of_file << endl;
 				return -1;
 			}
-			
-			file1.open(name_of_file1, std::ofstream::app);
-			if (file1.is_open())
-			{
-				file1 << "Name of image: " << name_of_image << endl;
-				file1.close();
-			}
-			else
-			{
-				cout << "Failed to open file " << name_of_file1 << endl;
-				return -1;
-			}
+
 			// iteration through all the vector with detected droplets
-			for (int i = 0; i < drops.size(); i++) {
+			for (int i = 0; i < drops.size(); i++) 
+			{
 				Rect r = drops[i];
 				// calculating the center of droplet
 				Point center(drops[i].x + drops[i].width*0.5, drops[i].y + drops[i].height*0.5 + 3);
@@ -338,7 +237,6 @@ int main(int argc, const char **argv) {
 				Rect circleRectangle;
 				Mat circlePart = imgr2p(Range(tl.y,br.y), Range(tl.x, br.x));
 				vector<Vec3f> circlesDetected;
-
 				HoughCircles(circlePart, circlesDetected, HOUGH_GRADIENT, 1, 1, 180, 80, radius*0.7, radius*1.3);
 
 				int cIt = 0;
@@ -346,16 +244,16 @@ int main(int argc, const char **argv) {
 				double newRadiusAvg = 0;
 				int maxRadiusIndx = 0;
 				Point newCenter=Point(0,0);
-				for (cIt; cIt < circlesDetected.size(); cIt++)
-				{
-					newCenter.x += circlesDetected[cIt][0];
-					newCenter.y += circlesDetected[cIt][1];
-					newRadiusAvg += circlesDetected[cIt][2];
-			
-				}
 
-				if (circlesDetected.size())
+				if (circlesDetected.size() && circlesDetected[cIt][1]!=0 && circlesDetected[cIt][1]!=0 && circlesDetected[cIt][2]!=0)
 				{
+					for (cIt; cIt < circlesDetected.size(); cIt++) //if Hough transform found new circles we sum center and radiuses to calculate average
+					{
+						newCenter.x += circlesDetected[cIt][0];
+						newCenter.y += circlesDetected[cIt][1];
+						newRadiusAvg += circlesDetected[cIt][2];
+
+					}
 					newCenter.x /= circlesDetected.size();
 					newCenter.y /= circlesDetected.size();
 					newRadiusAvg /= circlesDetected.size();
@@ -383,179 +281,59 @@ int main(int argc, const char **argv) {
 
 			}
 
-			//Mat outputLow, outputHigh, outputSobel, outputSobel2, outputSobel3, outputSobel4;
-			////filter2D(image, outputLow, -1, kernelLow,Point(-1,-1),0,BORDER_REPLICATE);
-			////filter2D(image, outputHigh, -1, kernelHigh, Point(-1, -1), 0, BORDER_REPLICATE);
-			//filter2D(imgLCr2p, outputSobel, -1, sobelx1, Point(-1, -1), 0, BORDER_REPLICATE);
-			////filter2D(image, outputSobel2, -1, sobelx2, Point(-1, -1), 0, BORDER_REPLICATE);
-			//filter2D(outputSobel, outputSobel, -1, sobelx2, Point(-1, -1), 0, BORDER_REPLICATE);
-			//
-			//filter2D(imgLCr2p, outputSobel3, -1, sobelx3, Point(-1, -1), 0, BORDER_REPLICATE);
-			////filter2D(image, outputSobel4, -1, sobelx4, Point(-1, -1), 0, BORDER_REPLICATE);
-			//filter2D(outputSobel3, outputSobel3, -1, sobelx4, Point(-1, -1), 0, BORDER_REPLICATE);
-			//
-			//Mat out;
-			////bitwise_and(outputSobel, outputSobel3, out);
-			//out = outputSobel + outputSobel3;
-			//Mat hOut;
-			//out.convertTo(hOut, CV_8U);
-			//imshow("OutPut", out);
-			//vector<vector<Point>> cont;
-			//vector<Vec4i> hierarchy;
-			//threshold(out, out, 70, 255, THRESH_BINARY);
-			//
-			//for (int n = 0; n < centers.size(); n++)
-			//{
-			//	circle(out, centers[n], radiuses[n], Scalar(0), -1);
-			//}
-			//Mat cannyOut;
-			//Mat thresh;
-			//threshold(out, thresh, 254, 255, THRESH_BINARY);
-			//Canny(out, cannyOut, 170, 240, 7);
-			//imshow("Canny", cannyOut);
-			//imshow("Threshold", thresh);
-			//Mat dilate,erode;
-			//Mat openKernel = Mat(Size(2, 2), CV_8UC1, Scalar(1)); //zrobic znowu erode i dilate osobno :v
-			//Mat outOpen;
-			//morphologyEx(out, outOpen, MORPH_OPEN, openKernel,Point(-1,-1),2, BORDER_REPLICATE);
-			//rectangle(out, Rect(Point(0, 0), Point(out.cols - 1, out.rows - 1)), Scalar(255), 1);
-			////imshow("OutPutBinClose", outOpen);
-			//findContours(thresh, cont, hierarchy, CV_RETR_CCOMP, CV_CHAIN_APPROX_SIMPLE);
-
-
-			//Mat o = Mat(imgLC.size(), imgLC.type(), Scalar(0));
-			//
-			//for (int i = 0; i < cont.size(); i++)
-			//{
-			//	int contPointConuter=0;
-			//	Point contPoint,contPointPrev;
-			//	int d = 0;
-			//	if(contourArea(cont[i])>20 && hierarchy[i][2]!=-1)
-			//	{
-			//		for (int j = 0; j < cont[i].size(); j++)
-			//		{
-			//			if (cont[i][j].x == (thresh.cols - 1) || cont[i][j].x == 0)
-			//			{
-			//				contPointConuter++;
-			//				if (contPointConuter >= 4)
-			//				{
-			//					drawContours(o, cont, i, Scalar(255), -1);
-			//					break;
-			//				}
-			//			}
-			//		}
-			//		
-			//	}
-			//}
-
-			//imshow("Circles", o);
-
 			while (keepProcessing) {
-				imshow(windowName, img1);				
-				//imshow("With Lense Cleaning", img1LC);
-				key = waitKey(20);
-
-				//key = 'n';
-				// Drawing droplets marked by user
-				if (key == 'd') {
-					int numberOfCircles = floor(points.size() / 3);
-
-					for (int i = 0; i < numberOfCircles; i++) {
-						calculateCenter(points[3 * i + 0], points[3 * i + 1], points[3 * i + 2]);
-						circle(img1, S, radiusS, Scalar(255, 0, 0), 2);
-						centers.push_back(S);
-						radiuses.push_back(radiusS);
-					}
-					points.clear();
-					img1 = imread(argv[k], 1);
-					for (int i = 0; i < centers.size(); i++) {
-						circle(img1, centers[i], radiuses[i], Scalar(255, 0, 0), 2);
-
-					}
-
-				}
-
-				// refreshing the image
-				if (key == 'r') {
-					img1 = imread(argv[k], 1);
-					for (int i = 0; i < centers.size(); i++) {
-						circle(img1, centers[i], radiuses[i], Scalar(255, 0, 0), 2);
-
-					}
-				}
-
-				// removing droplets marked by user
-				if (key == 'e') {
-					bool keepProcessing = 1;
-					int n = 0;
-					int distance = 0;
-					int check = 0;
-					for (int i = 0; i < points_to_remove.size(); i++)
-					{
-						keepProcessing = 1;
-						n = 0;
-						while (keepProcessing) {
-							distance = (points_to_remove[i].x - centers[n].x)*(points_to_remove[i].x - centers[n].x) + (points_to_remove[i].y - centers[n].y)*(points_to_remove[i].y - centers[n].y);
-							check = radiuses[n] * radiuses[n] - distance;
-							if (check > 0)
-							{
-								centers.erase(centers.begin() + n);
-								radiuses.erase(radiuses.begin() + n);
-								keepProcessing = 0;
-								n = 0;
-							}
-							else
-								n = n + 1;
-
-						}
-
-					}
-
-
-					points_to_remove.clear();
-					img1 = imread(argv[k], 1);
-					for (int i = 0; i < centers.size(); i++) {
-						circle(img1, centers[i], radiuses[i], Scalar(255, 0, 0), 2);
-
-					}
-
-				}
-
+				//imshow(windowName, img1);
+				//key = waitKey(20);
+				key = 'n';
 
 				// Going to the next image or finishing programme if current image is the last one
 				if (key == 'n') {
-					imwrite(tempPath, img1);
+					imwrite(tempPath, img1,saveParams);
 					keepProcessing = false;
 				}
 
-				//imshow(windowName, img1);
-				//string fName=to_string(k);
-				//
-				//imwrite(fName+".jpg", img1);
 			}
 
 
-			// Saving information from current image in files
+			// Saving information from current image in files. Mean diameter, min, max and SMD
 			file.open(name_of_file, std::ofstream::app);
-			file << "Droplets sizes in pixels: " << endl;
-			for (int i = 0; i < radiuses.size(); i++) {
-				file << 2 * radiuses[i];
-				file << std::endl;
+			file << "Nb : " << radiuses.size() << endl;
+			double meanDiameter = 0;
+			double maxDiameter = 0;
+			double minDiameter=1000;
+			vector<double> diamTmp;
+			for (int m = 0; m < radiuses.size(); m++)
+			{
+				meanDiameter += radiuses[m] * 2;
+				if (maxDiameter < radiuses[m] * 2)
+				{
+					maxDiameter = radiuses[m] * 2;
+				}
+				if (minDiameter > radiuses[m] * 2)
+				{
+					minDiameter = radiuses[m] * 2;
+				}
+				diamTmp.push_back(calibration_factor * 2 * radiuses[m]);
+				diameters_in_mikrometers.push_back(calibration_factor * 2 * radiuses[m]);
 			}
+			double d3tmp = 0;
+			double d2tmp = 0;
+			double SMDtmp = 0;
+			for (int d = 0; d < diamTmp.size(); d++) {
+				d3tmp = d3tmp + diamTmp[d] * diamTmp[d] * diamTmp[d];
+				d2tmp = d2tmp + diamTmp[d] * diamTmp[d];
+			}
+			SMDtmp = d3tmp / d2tmp;
+			file << "SMD : " << SMDtmp << endl;
+			file << "Mean : " << (calibration_factor * (meanDiameter/radiuses.size())) << endl;
+			file << "Min : " << calibration_factor*minDiameter << endl;
+			file << "Max : " << calibration_factor*maxDiameter << endl;
 			file.close();
 
-			file1.open(name_of_file1, std::ofstream::app);
-			file1 << "Droplets sizes in mikro meters : " << endl;
-			for (int i = 0; i < radiuses.size(); i++) {
-				file1 << calibration_factor * 2 * radiuses[i];
-				file1 << std::endl;
-				diameters_in_mikrometers.push_back(calibration_factor * 2 * radiuses[i]);
-			}
-			file1.close();
 			number_of_drops = number_of_drops + radiuses.size();
-
 		}
 		// Cleaning all the vectors
+		
 		centers.clear();
 		radiuses.clear();
 		points_to_remove.clear();
@@ -567,21 +345,36 @@ int main(int argc, const char **argv) {
 	d3 = 0;
 	d2 = 0;
 	SMD = 0;
+	double meanTotal=0;
+	double minTotal=1000;
+	double maxTotal=0;
 	for (int i = 0; i < diameters_in_mikrometers.size(); i++) {
 		d3 = d3 + diameters_in_mikrometers[i] * diameters_in_mikrometers[i] * diameters_in_mikrometers[i];
 		d2 = d2 + diameters_in_mikrometers[i] * diameters_in_mikrometers[i];
+		meanTotal += diameters_in_mikrometers[i];
+		if (maxTotal < diameters_in_mikrometers[i] )
+		{
+			maxTotal = diameters_in_mikrometers[i] ;
+		}
+		if (minTotal > diameters_in_mikrometers[i])
+		{
+			minTotal = diameters_in_mikrometers[i];
+		}
 	}
 	SMD = d3 / d2;
+	auto end = chrono::system_clock::now();
 
 	// Sauter Mean Diameter in file : "diametersInMikroMeters.txt"
-	file1.open(name_of_file1, std::ofstream::app);
-	file1 << "Number of all droplets:  " << number_of_drops << endl;
-	file1 << "Sauter mean diameter d32 =  " << SMD << endl;
-	file1.close();
-
 	file.open(name_of_file, std::ofstream::app);
-	file << "Number of all droplets:  " << number_of_drops << endl;
+	
+	file << endl<< "NbAll : " << number_of_drops << endl;
+	file << "SMDAll : " << SMD << endl;
+	file << "MeanAll : " << (meanTotal / diameters_in_mikrometers.size()) << endl;
+	file << "MinAll : " << minTotal << endl;
+	file << "MaxAll : " << maxTotal << endl;
+	file << "Time : " << chrono::duration_cast<chrono::seconds>(end - start).count()<<" sec"<<endl;
 	file.close();
+
 	cout << "Program finished without errors" << endl;
 	return 0;
 }
